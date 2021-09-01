ffecbd9f14c56a34815f23cb07e049e81ae091c4
Mateus Elias <meap@cin.ufpe.br>
2021-09-01 09:16:48 -0300

    Update CameraPlayer.gd

--- Scripts/CameraPlayer.gd
+++ Scripts/CameraPlayer.gd
@@ -2,6 +2,6 @@ extends Camera2D
 
 onready var player = get_node("/root/FirstDay/YSort/Player")
 
-func _process(delta):
+func _process(_delta):
 	position.x = player.position.x
 	position.y = player.position.y
